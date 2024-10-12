d8c38dca9586515b43fe610650eda62b63fb1444
Tony Jin <yuhueijin@gmail.com>
2024-10-12 10:43:09 +0800

    build api with express

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var logger = require('morgan');
+
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/users', usersRouter);
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+module.exports = app;
